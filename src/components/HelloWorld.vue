76b5f2a0a9f9a06698297a60e67213cde2a8958d
matthewsteedman <msteedman77@gmail.com>
2021-06-04 15:52:08 +0200

    Added a button to view top headlines and add the backend i created with node to th front end

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,152 @@
 <template>
-<h3>Top Headlines</h3>
+    <h3>TODAYS TOP HEADLINES</h3>
+    <!-- input for the search function and the button to make it function -->
+    <div class="search_form">
+      <q-form
+            @submit="onSubmit"
+            class="q-gutter-md"
+            
+            >
+              <q-input 
+              filled 
+              placeholder="Search" 
+              class="search"
+              autocorrect="on"
+              autocomplete="on"
+              v-model= data.title
+              hint="china, testa, bitcoin"
+              ></q-input>
 
+              <q-btn 
+              type="submit"
+              class="search_btn" 
+              :loading="loading[0]" 
+              color="secondary" 
+              @click="simulateProgress(0)" 
+              label="Search" />
+      </q-form>
+    </div>  
+        <q-form  @submit="all_articles"
+                  class="q-gutter-md">
 
-      <q-input
-        filled
-        placeholder="Search"
-        class="search"
-      >
-       <template v-slot:append>
-          <q-icon name="search" />
-        </template>
-      </q-input>
-      <q-btn class="search_btn"  label="Search" />
-      
+                  <q-btn 
+                    type="submit"
+                    class="topheadlins_btn" 
+                    :loading="loading[1]" 
+                    color="secondary" 
+                    @click="simulateProgress(1)" 
+                    label="Top Headliness" />
+          </q-form>
+    <!-- all articles -->
 
-  <q-page-container>
-    <div class="parent_div">
-      <q-item v-for="articles in items" :key="articles.id">
-     <q-card class="my-card">
-       <img :src="articles.urlToImage">
-      <q-card-section>
-        <h6>{{ articles.author }}</h6>
-        <div class="text-subtitle2">{{ articles.description }}</div>
-      </q-card-section>
+    <div class="row blog_container" v-if="items">
+        <q-item class="col-xs-12 col-sm-6 col-md-4" v-for="articles in items" :key="articles.id">
+          <q-card class="my-card">
+            <img :src="articles.urlToImage">
+            <q-card-section>
+              <h6>{{ articles.title }}</h6>
+              <div class="text-subtitle2">{{ articles.description }}</div>
+            </q-card-section>
 
-      <q-card-section class="q-pt-none">
-        {{ articles.publishedAt }}
-      </q-card-section>
-    </q-card>
-    </q-item>
+            <q-card-section class="q-pt-none">
+              {{ articles.publishedAt }}
+            </q-card-section>
+          </q-card>
+        </q-item>
      </div>
-    </q-page-container>
-  
 </template>
+
+<script>
+import { ref } from 'vue'
+
+const data = ({ref})
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      items: undefined,
+    }
+  },
+  created(){
+    this.all_articles()
+  },
+  methods: {
+    // function to get all articles
+
+   all_articles(){
+        try {
+          const url = "http://localhost:8000/api/articles";
+          fetch(url)
+          .then((response) => response.json())
+          .then((json) => {
+            console.log(json.stringify);
+            this.items = json.articles
+              console.log("ITEMS", this.items)
+            })
+        } catch (error) {
+          console.log("Server Error")
+        }
+    },
+    // function to search through articles
+
+   onSubmit (){
+          try {
+            fetch(`http://localhost:8000/api/search`, {
+              method: "POST",
+              body: JSON.stringify(this.data),
+              headers: {
+                "Content-Type": "application/json"
+              }
+            })
+                .then((response) => response.json())
+                .then((json) => {
+                  if (this.data.title){
+                    this.items = json.articles
+                  }
+                  
+                  })
+          } catch (error) {
+            console.log(error)
+          }
+      }
+  },
+
+  setup(){
+    const loading = ref([
+      false,
+    ])
+    const progress = ref(false)
+
+     function simulateProgress (number) {
+      // we set loading state
+      loading.value[ number ] = true
+
+      // simulate a delay
+      setTimeout(() => {
+        // we're done, we reset loading state
+        loading.value[ number ] = false
+      }, 3000)
+     }
+    return {
+      items: ref([]),
+      filtered_data: ref([]),
+      loading,
+      progress,
+      simulateProgress,
+      data,
+       skeletonTypes: [
+        'text', 'rect', 'circle',
+        'QBtn', 'QBadge', 'QChip', 'QToolbar',
+        'QCheckbox', 'QRadio', 'QToggle',
+        'QSlider', 'QRange', 'QInput',
+        'QAvatar'
+      ]
+    }
+  }
+}
+
+</script>
 
 <style>
 body{
@@ -40,14 +154,26 @@ body{
 }
 .my-card{
   width: 100%;  
-  max-width: 500px;
- 
+  max-width: 400px;
+  
 }
-.search_btn{
-  color: blue;
-  align-content: center;
+.blog_container{
+  width: 100%;
 }
-.search{
+.search_form{
+  margin: 20px;
+}
+.q-gutter-md .topheadlins_btn{
+   margin-right: auto;
+  margin-left: auto;
+  display: block;
+}
+.q-gutter-md .search_btn{
+  margin-right: auto;
+  margin-left: auto;
+  display: block;
+}
+.q-gutter-md .search{
   width: 25%;
   display: block;
   margin-right: auto;
@@ -56,43 +182,17 @@ body{
 .parent_div{
   margin: 0 -5px;
 }
+h6{
+  font-weight: 200;
+  font-family: sans-serif;
+  text-transform: capitalize;
+}
 
 h3{
+  font-weight: 200;
   text-align: center;
   font-family: sans-serif;
+  text-transform: capitalize;
 }
 
 </style>
-
-<script>
-export default {
-  name: 'HelloWorld',
-    created(){
-    this.all_articles()
-  },
-  methods: {
-      all_articles(){
-        try {
-          const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=dd4c8ce956fb4fa98ada252f4e25153d";
-          fetch(url)
-          .then((response) => response.json())
-          .then((json) => {
-            console.log(json)
-            this.items = json.articles
-            console.log("ITEMS",this.items)
-             })
-        } catch (error) {
-          console.log("Server Error")
-          
-        }
-    }
-  },
-
-  setup(){
-    return {
-      items: []
-    }
-  }
-}
-
-</script>
